228026ed43cf2262bb954bd92651bbb746022db9
a195281 <195281@lci-13-36.UPF>
2023-06-06 19:44:24 -0300

    teste

--- Poo.java
+++ Poo.java
@@ -1,19 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package poo;
 
-/**
- *
- * @author 195281
- */
 public class Poo {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+public static void main(String[] args) {
         System.out.println("Hello World");
 
     }
